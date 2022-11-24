2274930e6bda4039837d9f99c56875e23ca60ad8
matthew-via-music <mvm@matthewviamusic.com>
2022-11-24 19:09:27 +0000

    configure for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://matthew-via-music.com/blog/',
-  base: '/',
+  site: 'https://matthew-via-music.github.io',
+  base: '/blog',
 });
